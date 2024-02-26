d6e53e35362ffce8226e43b55d46c4003f84ac7e
Yash Mayekar <139975704+YashMayekar@users.noreply.github.com>
2024-02-26 21:08:00 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Rating from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Rating />
+  </React.StrictMode>
+);
